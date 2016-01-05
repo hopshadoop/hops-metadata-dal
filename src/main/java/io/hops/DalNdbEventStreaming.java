cd61a4dec839049a46d8beda6564c18e4593827f
Gautier Berthou <gautier.berthou@gmail.com>
2016-01-05 16:18:38 +0100

    Preparation to enable failover in distributed mode: - identify pending events by their id and the node that emited them (the id is unique only per node) - use the latest version of the streaming library

--- src/main/java/io/hops/DalNdbEventStreaming.java
+++ src/main/java/io/hops/DalNdbEventStreaming.java
@@ -19,13 +19,12 @@ package io.hops;
  *
  * @author sri
  */
-
 public interface DalNdbEventStreaming {
 
-    public void startHopsNdbEvetAPISession();
+  public void startHopsNdbEvetAPISession(boolean isLeader);
 
-    public void closeHopsNdbEventAPISession();
+  public void closeHopsNdbEventAPISession();
 
-    public boolean isNativeCodeLoaded();
+  public boolean isNativeCodeLoaded();
 
 }
