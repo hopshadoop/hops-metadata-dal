fa91b0323ad4a913012e60de70c30c4d5c2203a4
Gautier Berthou <gautier.berthou@gmail.com>
2016-04-12 15:48:43 +0200

    remove unused JustLaunchedContainers

--- src/main/java/io/hops/metadata/yarn/entity/RMNodeComps.java
+++ src/main/java/io/hops/metadata/yarn/entity/RMNodeComps.java
@@ -27,7 +27,6 @@ public class RMNodeComps {
   private final Node hopNode;
   private final NodeHBResponse hopNodeHBResponse;
   private final Resource hopResource;
-  private final List<JustLaunchedContainers> hopJustLaunchedContainers;
   //hopUpdatedContainerInfo are grouped by updatedContainerInfoId
   private final List<UpdatedContainerInfo> hopUpdatedContainerInfo;
   private final List<ContainerId> hopContainerIdsToClean;
@@ -40,7 +39,6 @@ public class RMNodeComps {
           Node hopNode,
           NodeHBResponse hopNodeHBResponse, Resource hopResource,
           PendingEvent hopPendingEvent,
-          List<JustLaunchedContainers> hopJustLaunchedContainers,
           List<UpdatedContainerInfo> hopUpdatedContainerInfo,
           List<ContainerId> hopContainerIdsToClean,
           List<FinishedApplications> hopFinishedApplications,
@@ -51,7 +49,6 @@ public class RMNodeComps {
     this.hopNodeHBResponse = hopNodeHBResponse;
     this.hopResource = hopResource;
     this.hopPendingEvent = hopPendingEvent;
-    this.hopJustLaunchedContainers = hopJustLaunchedContainers;
     this.hopUpdatedContainerInfo = hopUpdatedContainerInfo;
     this.hopContainerIdsToClean = hopContainerIdsToClean;
     this.hopFinishedApplications = hopFinishedApplications;
@@ -78,10 +75,6 @@ public class RMNodeComps {
 
   public Resource getHopResource() {
     return hopResource;
-  }
-
-  public List<JustLaunchedContainers> getHopJustLaunchedContainers() {
-    return hopJustLaunchedContainers;
   }
 
   public List<UpdatedContainerInfo> getHopUpdatedContainerInfo() {
